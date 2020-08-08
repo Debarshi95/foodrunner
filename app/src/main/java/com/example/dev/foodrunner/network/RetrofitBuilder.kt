83c1adb0ef9f6658887644f0cdd47dd6a7772119
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2020-08-08 22:37:45 +0530

    splash theme, UI enhancement etc

---
+++ app/src/main/java/com/example/dev/foodrunner/network/RetrofitBuilder.kt
@@ -0,0 +1,4 @@
+package com.example.dev.foodrunner.network
+
+class RetrofitBuilder {
+}
--- app/src/main/java/com/example/dev/foodrunner/ui/restaurants/RestaurantFragment.kt
+++ app/src/main/java/com/example/dev/foodrunner/ui/restaurants/RestaurantFragment.kt
@@ -1,14 +1,11 @@
-package com.example.dev.foodrunner.ui.sign_up
+package com.example.dev.foodrunner.ui.restaurants
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
 import com.example.dev.foodrunner.R
-import com.example.dev.foodrunner.databinding.FragmentSignUpBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,15 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
+ * Use the [RestaurantFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SignUpFragment : Fragment() {
+class RestaurantFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var binding: FragmentSignUpBinding? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +35,7 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSignUpBinding.inflate(inflater, container, false)
-        return binding!!.root
+        return inflater.inflate(R.layout.fragment_restaurant, container, false)
     }
 
     companion object {
@@ -51,36 +45,16 @@ class SignUpFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
+         * @return A new instance of fragment RestaurantFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
+            RestaurantFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        navigateToRestaurantsFragment()
-        navigateToSignInFragment()
-    }
-
-
-    private fun navigateToRestaurantsFragment() {
-        binding!!.btnReg.setOnClickListener {
-            it.findNavController().navigate(R.id.action_signUpFragment_to_restaurantFragment)
-        }
-    }
-
-    private fun navigateToSignInFragment() {
-        binding!!.tvMoveToSignIn.setOnClickListener {
-            it.findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
-        }
     }
 }
